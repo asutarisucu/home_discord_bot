51ca977f29189e95ef361214546107d57d980191
asutarisucu <126184080+asutarisucu@users.noreply.github.com>
2024-02-08 00:23:32 +0900

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import discord
-
-TOKEN:str="MTE5OTU5MTkyNDEzOTU2OTE5Mg.GwtH6q.KCpvPVq-omxrcWLUt2otjiUUacXoEMDT6UKPvk"
-LOG_CHANNEL:str="1170985511532376184"
-LOGIN_EMBED=discord.Embed(
-    color=0x7cfc00,
-    title="Login_Msg"
-)
