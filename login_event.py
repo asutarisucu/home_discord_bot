54f880981766c2f91d218cfa5ebf8ee505eb6a50
asutarisucu <asutaricusu.s@gmail.com>
2024-02-10 01:10:43 +0900

    ログインメッセージにディスク使用量を追加

--- login_event.py
+++ login_event.py
@@ -1,5 +1,6 @@
 import discord
 import config
+import shutil
 def login_event(client,tree):
     @client.event
     async def on_ready():
@@ -8,6 +9,30 @@ def login_event(client,tree):
         for i in range(len(channel_list)):
             if str(channel_list[i].id) == config.LOG_CHANNEL:
                 channel = channel_list[i] 
+        embed=create_embed()
         await channel.send(embed=config.LOGIN_EMBED)##指定したチャンネルへの出力
         
         await tree.sync()##コマンドの同期
+
+def create_embed():
+    embed=config.LOGIN_EMBED
+    disksize=get_disk_size()
+    embed.add_field(name="ディスク使用量",value=disksize)
+    
+def get_disk_size():
+    total, used, free = shutil.disk_usage('/')
+    try:
+        used=float(used)
+        kb=used/1024
+    except:
+        return"予期せぬエラーが発生しました"
+    if kb>=1024:
+        mb=kb/1024
+        if mb>=1024:
+            gb=mb/1024
+            return "%.2fGB" %gb
+        else:
+            return "%.2fMB" %mb
+    else:
+        return "%.2fKB" %kb
+    
