f530ec35f9e6ce058d6ed3b92bc07986a0492e08
asutarisucu <asutaricusu.s@gmail.com>
2024-02-14 07:35:07 +0900

    webコマンド

--- start_web.py
+++ start_web.py
@@ -4,10 +4,14 @@ def web_command(Interaction,tree):
     @tree.command(name='web',description='webサーバーに関するコマンドです')
     async def start(interaction: Interaction,text:str):
         commands = {
-        'start':"sudo systemctl start apache2"
+        'start':"sudo systemctl start apache2",
+        'stop':"sudo systemctl stop apache2",
+        'restart':"sudo systemctl restart apache2",
+        'update':["cd /home/asuta/scripts","bash update_web.sh"]
         }
         if text in commands:
-            os.system(commands[text])
+            for cm in commands:
+                os.system(commands[text])
             await interaction.response.send_message('コマンドが実行されました')
         else:
             await interaction.response.send_message('無効な引数です')
