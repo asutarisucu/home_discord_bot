070100e45d2559976187e659b610200bf88987dd
asutarisucu <asutaricusu.s@gmail.com>
2024-02-14 01:28:53 +0900

    webコマンド

--- start_web.py
+++ start_web.py
@@ -1,12 +1,13 @@
 import discord
 import os
 def web_command(Interaction,tree):
-    @tree.command(name='start',description='webサーバーを起動します')
+    @tree.command(name='web',description='webサーバーに関するコマンドです')
     async def start(interaction: Interaction,text:str):
         commands = {
         'start':"sudo systemctl start apache2"
         }
         if text in commands:
             os.system(commands[text])
+            await interaction.response.send_message('コマンドが実行されました')
         else:
-            interaction.response.send_message('無効な引数です')
+            await interaction.response.send_message('無効な引数です')
